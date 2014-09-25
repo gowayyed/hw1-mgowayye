04685d022c2ca5671dc549a6c23c08a77f7bb1b2
gowayyed <gowayyed@cs.cmu.edu>
2014-09-24 23:59:46 -0400

    version 2.4

--- hw1-mgowayye/src/main/java/misc/Config.java
+++ hw1-mgowayye/src/main/java/misc/Config.java
@@ -17,16 +17,16 @@ public class Config {
    * The path of the file that stores the {@link CRF} model. I originally saved the model on the
    * src/main/resources/data. I am not sure where it will be on the server.
    */
-  public static String modelFile = "src/main/resources/data/model";
+//  public static String modelFile = "src/main/resources/data/model";
 
-  // public static String modelFile = "smodel";
+   public static String modelFile = "smodel";
 
   /**
    * The path of the original file that we want to process, used by {@link BioCollectionReader}
    */
-  public static String inputPath = "src/main/resources/data/sample.in";
+//  public static String inputPath = "src/main/resources/data/sample.in";
 
-  // public static String inputPath = "hw1.in";
+   public static String inputPath = "hw1.in";
   /**
    * The path of the output file to save the predictions of the model. Used by
    * {@link TestingAnnotator}
